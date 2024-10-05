ff931450ae32ec1cd5a7070aff5495df60da4d26
TonqiS <priv.thanglongnguyen@gmail.com>
2024-10-05 22:28:39 +0200

    Error handling

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 function CalculateGGT(){
 var x = parseInt(document.getElementById('num1').value)
 var y = parseInt(document.getElementById('num2').value)
+if(isNaN(x) || isNaN(y)){
+    alert('Bitte beide Zahlen eingeben')
+    return
+}
 if(x % y == 0 || y % x == 0){
     if(x < y){
         alert('GGT: ' + x)
