32fd5522195ddaa5ae8c2a68010af4299618d37d
TonqiS <priv.thanglongnguyen@gmail.com>
2024-10-05 22:17:59 +0200

    Little change

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ var x = parseInt(document.getElementById('num1').value)
 var y = parseInt(document.getElementById('num2').value)
 if(x % y == 0 || y % x == 0){
     if(x < y){
-        alert(x)
+        alert('GGT:' + x)
         return
         }
         else{
-            alert(y)
+            alert('GGT:' + y)
             return
         }
     }
@@ -16,7 +16,7 @@ else{
     while(rest!= 0){
         rest =x%y
         if(rest == 0){
-            alert(y)
+            alert('GGT:' + y)
             return
             }
         x = y
